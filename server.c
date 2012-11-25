d0f85b3e5c8fbd791b5929f00301a8fee7d7947b
hhamilto <hhamilto@mtu.edu>
2012-11-25 12:54:15 -0500

    added a make file and continued work on the server. mostly have the skeleton there

--- server.c
+++ server.c
@@ -13,11 +13,23 @@
 #include <sys/select.h>
 #include <sys/time.h>
 
+typedef struct spawn_t{
+	int pipe[2];
+	int guesses;
+	char name[30];
+} spawn;
+
 
 int main(void){
+	spawn childs[101];
+	// to keep track of where our unused pipe spots are
+	int childi = 0;
+	memset(childs, 0, sizeof(spawn) * 101);
     // set up socket (in process)
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
+	int i;
+	
     // create destination
     struct sockaddr_in s_in;
     memset((char *) &s_in, sizeof(s_in),0); 
@@ -45,27 +57,49 @@ int main(void){
 	FD_SET(0,&read_set);
 	while(1){
 		//int select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
-		select(socket_fd + 1, &read_set, NULL, NULL, NULL);
-		
+		int num_active = select(socket_fd + 1, &read_set, NULL, NULL, NULL);
+		if( num_active > 1){
+			printf("select behaving wierd\n"); exit(1);
+		}
 		if(FD_ISSET(0,&read_set)){
 			// got somthing on 
 			printf("select got unplugged by stds in\n");
 			exit(1);
-		}else if(FD_ISSET(0,&read_set)){
 			
-			printf("select got unplugged by stds in\n");
+		}else if(FD_ISSET(socket_fd,&read_set)){
 			struct sockaddr_in s_out;
 			length = sizeof(s_out);
 		
 			// accept a connection
 			int conn_fd = accept(socket_fd, (struct sockaddr*)&s_out, &length);
 			if (conn_fd < 0) { perror("accept"); exit(1); }
-		
+			
+			if(pipe(childs[childi].pipe)){
+				perror("pipin"); exit(1);
+			}
+			
 			printf("fork\n");
-			//int cpid = fork();
-			exit(1);
+			int cpid = fork();
+			if(cpid){
+				//child
+				exit(1);
+			}else{
+				
+			}
+			FD_SET(socket_fd,&read_set);
+		}else{
+			//must be one of our children dieing!
+			
+			// wait for it
 		}
-		
-		
+		// reset the fd set
+		FD_ZERO(&read_set);
+		FD_SET(socket_fd,&read_set);
+		FD_SET(0,&read_set);
+		for(i = 0; i < childi; i++){
+			if(childs[i].guesses == 0){
+				FD_SET(childs[i].pipe[1],&read_set);
+			}
+		}
 	}
 }
